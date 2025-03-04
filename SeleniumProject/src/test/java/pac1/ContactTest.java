3f2b2dc7b930b2efffacc54b14f94dc8289200ba
DELL <DELL@DESKTOP-23IQLTI>
2025-03-04 15:25:56 +0530

    created delete

--- SeleniumProject/src/test/java/pac1/ContactTest.java
+++ SeleniumProject/src/test/java/pac1/ContactTest.java
@@ -10,8 +10,6 @@ public class ContactTest {
 		String USERNAME=System.getProperty("username");
 		String PASSWORD=System.getProperty("password");
 		
-		System.out.println(URL);
-		System.out.println(BROWSER);
 		System.out.println(USERNAME);
 		System.out.println(PASSWORD);
 		System.out.println("execute createContactTest");
@@ -21,5 +19,11 @@ public class ContactTest {
 	public void modifyContactTest() {
 		System.out.println("execute modifyContactTest");
 	}
+	
+	@Test
+	public void deleteContactTest() {
+		System.out.println("execute deleteContactTest");
+	}
+
 
 }
--- SeleniumProject/src/test/java/pac2/OrgTest.java
+++ SeleniumProject/src/test/java/pac2/OrgTest.java
@@ -10,10 +10,12 @@ public class OrgTest {
 		String USERNAME=System.getProperty("username");
 		String PASSWORD=System.getProperty("password");
 		
+
 		System.out.println(URL);
 		System.out.println(BROWSER);
 		System.out.println(USERNAME);
 		System.out.println(PASSWORD);
+		
 		System.out.println("execute createOrgTest");
 	}
 	
@@ -21,6 +23,9 @@ public class OrgTest {
 	public void modifyOrgTest() {
 		System.out.println("execute modifyOrgTest");
 	}
+	
+	
+	
 
 
 }
